0d323a660af9cfd9601c344e769bedab58187de9
joacim <joacim.astrom@gmail.com>
2016-03-04 15:17:49 +0100

    Add hello

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,8 @@ Rails.application.routes.draw do
   #     end
   #   end
 
+  root 'application#hello'
+
   # Example resource route with sub-resources:
   #   resources :products do
   #     resources :comments, :sales
