7660e044d3f8da3fd5355ced14cef0f0dff9d0aa
joacim <joacim.astrom@gmail.com>
2016-03-12 17:37:22 +0100

    finished login/logout

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -23,7 +23,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert is_logged_in?
+
   end
+
+
+
 
   
 end
