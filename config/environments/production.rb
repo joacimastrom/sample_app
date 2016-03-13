d7d83f38dc802af32bfe48d088cfdd3fb7d22b1a
joacim <joacim.astrom@gmail.com>
2016-03-14 00:38:47 +0100

    bugfix2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<jocke>.herokuapp.com'
+  host = 'jocke.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
