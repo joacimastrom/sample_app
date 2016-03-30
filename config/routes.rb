db7017b71a91f99205feb71dc3df86badcf7a1cd
joacim <joacim.astrom@gmail.com>
2016-03-30 23:53:24 +0200

    update for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   
- 
-
   get 'password_resets/new'
-
   get 'password_resets/edit'
 
   root             'static_pages#home'
@@ -24,6 +21,5 @@ Rails.application.routes.draw do
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
-
 
 end
